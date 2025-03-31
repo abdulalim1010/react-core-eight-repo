2cba84ae3eb6155aeca6a5580b226a9cfc867418
abdulalilm1010 <abdulalim243457@gmail.com>
2025-03-31 22:57:34 +0600

    day on the hard

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+  
+  const bottoles = [
+  {id:1,name:'pink bottole',price:455,color:'red'},
+  {id:1,name:'chine bottole',price:455,color:'red'},
+  {id:1,name:'alim',price:455,color:'red'},
+  {id:1,name:'alim',price:455,color:'red'},
+]
+  return (
+    <>
+  
+      <h1>Buy Awesome Water Bottole</h1>
+      
+    </>
+  )
+}
+
+export default App
