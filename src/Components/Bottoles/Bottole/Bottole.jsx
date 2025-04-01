3356a628a3276baefee3c1c84cf72211d2ac9d6c
abdulalilm1010 <abdulalim243457@gmail.com>
2025-04-01 22:30:42 +0600

    add button

--- src/Components/Bottoles/Bottole/Bottole.jsx
+++ src/Components/Bottoles/Bottole/Bottole.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './Bottole.css'
 import Bottoles from '../Bottoles';
-const Bottole = ({ bottle }) => {
-  const { img } = bottle;
-  console.log(bottle)
+const Bottole = ({ bottle,handleAddToCart }) => {
+  const { img,name,price,quantity,seller,
+shipping,stock
+ } = bottle;
+  //console.log(bottle)
   return (
-    <div className='card'>
-      <img src={bottle.img} alt="" />
-     
-     
-    </div>
+    <div className='card bottle'>
+      <img src={img} alt="" />
+      <div style={{padding:"10px"}}><h3>{name}</h3>
+      <p>{price}</p>
+      <h4>{quantity}</h4>
+      <h3>{seller}</h3>
+      <p>{ shipping}</p>
+      <p>{stock}</p>
+      <button onClick={handleAddToCart}>Buy Now</button></div>
+</div>
   );
 };
 
--- src/Components/Bottoles/Bottoles.jsx
+++ src/Components/Bottoles/Bottoles.jsx
@@ -1,18 +1,23 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 
 import Bottole from './Bottole/Bottole';
 
 
 const Bottoles = ({ bottolesPromise }) => {
-  
+  const [cart, setCart] = useState([]);
   const bottole = use(bottolesPromise)
-  console.log(bottole)
+  const handleAddToCart = () => {
+    console.log('bottole will to added to cart');
+  }
+  //console.log(bottole)
   return (
     <div>
       <h2>Bottole:{bottole.length}</h2>
-<div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)" }}>
+      <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap:"10px",borderRadius:"10px" }}>
       {
-       bottole.map(bottle=><Bottole key={bottle.id} bottle={bottle}></Bottole>)
+          bottole.map(bottle => <Bottole key={bottle.id}
+        handleAddToCart={handleAddToCart}
+            bottle={bottle}></Bottole>)
       }</div>
     </div>
   );
